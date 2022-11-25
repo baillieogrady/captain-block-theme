74645bebd9684792e0ae89160c6525d64d32466c
Baillie O'Grady <baillieogrady@Baillies-MacBook-Air.local>
2022-11-25 20:08:17 +0000

    Add card pattern

---
+++ patterns/card.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+    * Title: Card
+    * Slug: captain/card
+    * Categories: captain
+    * Block types: core
+    */
+?>
+
+<!-- wp:query {"queryId":11,"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false, "className":"lg:hover:rotate-[4deg]"}} -->
+<div class="wp-block-query lg:hover:rotate-[4deg]">
+    <!-- wp:post-template -->
+        <!-- wp:group {"className":"mb-4"} -->
+            <div class="wp-block-group mb-4">
+                <!-- wp:post-featured-image {"isLink":true,"align":"wide"} /-->
+            </div>
+        <!-- /wp:group -->
+        <!-- wp:post-title {"isLink":true, "level": 3} /-->
+    <!-- /wp:post-template -->
+    </div>
+<!-- /wp:query -->
+
