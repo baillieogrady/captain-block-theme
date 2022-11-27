289a1ab48dc5c600c6b7234feab9fe307fa2407f
Baillie O'Grady <baillieogrady@Baillies-MacBook-Air.local>
2022-11-27 12:14:12 +0000

    Add overview pattern

---
+++ patterns/overview.php
@@ -0,0 +1,26 @@
+<?php
+    /*
+    * Title: Overview
+    * Slug: captain/overview
+    * Categories: captain
+    * Block types: ['core','captain']
+    */
+?>
+
+<!-- wp:captain/grid -->
+<div class="wp-block-captain-grid">
+    <div class="grid grid-cols-4 lg:grid-cols-12 gap-x-5">
+        <!-- wp:captain/grid-item -->
+        <div class="wp-block-captain-grid-item col-span-4 lg:col-span-7">
+            <!-- wp:paragraph -->
+            <p>American soccer magazine for over ten years transitioned to online-only. I helped convert their bold new web designs to a WordPress website that allowed for exclusive content and an elegantly displayed magazine interface for a myriad of devices.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph -->
+            <p>Overall, the process took 15 working days including the migration of their current archives</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:captain/grid-item -->
+    </div>
+</div>
+<!-- /wp:captain/grid -->
