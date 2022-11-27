83467a949e81a18d67eb2ff78d4dd90fdc7ae6a8
Baillie O'Grady <baillieogrady@Baillies-MacBook-Air.local>
2022-11-27 11:56:48 +0000

    Add Jumbotron pattern

--- patterns/jumbotron.php
+++ patterns/jumbotron.php
@@ -1,12 +1,22 @@
 <?php
     /*
     * Title: Jumbotron
-    * Slug: minimo/jumbotron
-    * Categories: minimo
-    * Block types: core
+    * Slug: captain/jumbotron
+    * Categories: captain
+    * Block types: ['core','captain']
     */
 ?>
 
-<!-- wp:image {"id":109,"sizeSlug":"large","linkDestination":"none","className":"jumbotron"} -->
-<figure class="wp-block-image size-large jumbotron"><img src="http://baillie-ogrady.local/wp-content/uploads/2022/11/what-a-howler-1024x576.jpg" alt="" class="wp-image-109"/></figure>
+<!-- wp:captain/container -->
+<div class="wp-block-captain-container">
+    <!-- wp:heading {"level":1,"className":"mb-4 lg:mb-8"} -->
+    <h1 class="mb-4 lg:mb-8">What a howler</h1>
+    <!-- /wp:heading -->
+</div>
+<!-- /wp:captain/container -->
+
+<!-- wp:image {"align":"full","sizeSlug":"full","linkDestination":"none","className":"is-style-default br-all__none"} -->
+<figure class="wp-block-image alignfull size-full is-style-default br-all__none">
+    <img alt=""/>
+</figure>
 <!-- /wp:image -->
