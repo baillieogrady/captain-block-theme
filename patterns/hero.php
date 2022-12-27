a71604592c2fde1187c213a06314922cc1f3c835
baillie <hello@baillieogrady.com>
2022-12-27 15:55:54 +0000

    Add hero pattern and single post template

---
+++ patterns/hero.php
@@ -0,0 +1,28 @@
+<?php
+    /*
+    * Title: Hero
+    * Categories: captain
+    * Slug: captain/hero
+    */
+?>
+
+
+<!-- wp:captain/spacer -->
+<div class="wp-block-captain-spacer h-22 lg:h-36"></div>
+<!-- /wp:captain/spacer -->
+
+<!-- wp:heading {"level":1} -->
+<h1>Creating custom<br>WordPress themes</h1>
+<!-- /wp:heading -->
+
+<!-- wp:captain/grid -->
+<div class="wp-block-captain-grid"><div class="grid grid-cols-12 gap-x-5"><!-- wp:captain/grid-item {"columnEnd":5} -->
+<div class="wp-block-captain-grid-item col-start-1 col-end-12 lg:col-start-undefined lg:col-end-5"><!-- wp:captain/lead -->
+<div class="wp-block-captain-lead"><p placeholder="Add text..." class="lg:text-2xl">Freelance WordPress developer based in Manchester, England. Working with Web agencies to transform their client's designs to fast WordPress themes.</p></div>
+<!-- /wp:captain/lead --></div>
+<!-- /wp:captain/grid-item --></div></div>
+<!-- /wp:captain/grid -->
+
+<!-- wp:captain/spacer {"height":"h-32 lg:h-64"} -->
+<div class="wp-block-captain-spacer h-32 lg:h-64"></div>
+<!-- /wp:captain/spacer -->
