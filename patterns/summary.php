cb841de508f471d8dc02788fb758fbc9520c3e4e
baillie <hello@baillieogrady.com>
2022-12-27 17:18:40 +0000

    Add summary pattern

---
+++ patterns/summary.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    * Title: Summary
+    * Categories: captain
+    * Slug: captain/summary
+    */
+?>
+
+<!-- wp:captain/grid -->
+<div class="wp-block-captain-grid"><div class="grid grid-cols-12 gap-x-5"><!-- wp:captain/grid-item {"columnEnd":5} -->
+<div class="wp-block-captain-grid-item col-start-1 col-end-12 lg:col-start-undefined lg:col-end-5"><!-- wp:captain/lead -->
+<div class="wp-block-captain-lead"><p placeholder="Add text..." class="lg:text-2xl">Freelance WordPress developer based in Manchester, England. Working with Web agencies to transform their client's designs into fast WordPress themes.</p></div>
+<!-- /wp:captain/lead --></div>
+<!-- /wp:captain/grid-item --></div></div>
+<!-- /wp:captain/grid -->
+
+<!-- wp:captain/spacer {"height":"h-32 lg:h-64"} -->
+<div class="wp-block-captain-spacer h-32 lg:h-64"></div>
+<!-- /wp:captain/spacer -->
